67e27a3d85ff1b9d9c93a44766ffc180815ad07a
CoriaEzequiel <zequielcoria@gmail.com>
2025-08-13 13:54:29 -0300

    Render Template Condicional

---
+++ Flask/2_Condicionales/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route("/condicionales")
+def condicional():
+    return render_template("condicional.html", equipo="Barcelona")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
