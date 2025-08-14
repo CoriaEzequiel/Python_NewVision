d9fbdd0f8ab19cd50e286e5ebed02de985864edc
CoriaEzequiel <zequielcoria@gmail.com>
2025-08-14 17:01:34 -0300

    Route and Query Parameters in FastApi

--- FastApi/main.py
+++ FastApi/main.py
@@ -14,18 +14,44 @@ movies = [          #lista de diccionarios
         "year":"2009",
         "rating": 7.9,
         "category": "Ficción"
+    },
+    {
+        "id":2,
+        "title": "Batman",
+        "overview": "Sumergete en las profundidades de Gotham City",
+        "year":"1989",
+        "rating": 8.2,
+        "category": "Acción"
     }
 ]
 
 
-@app.get('/', tags=['Home']) #añado atributos al endpoint (con tags agrupo endpoints)
-def home():
-    return "Welcome" #respuesta string
+#@app.get('/', tags=['Home']) #añado atributos al endpoint (con tags agrupo endpoints)
+#def home():
+#    return "Welcome" #respuesta string
 
 
 @app.get('/movies', tags=['Home'])
-def home():
+def get_movies():
     return movies
    # return {"Hello":"world"} #respuesta diccionario
    # return HTMLResponse('<h1>List of movies</h1>') #html response
     
+
+#PARAMETROS DE RUTA: valores que puedo pasar dentro de la URL
+@app.get('/movies/{id}', tags=['Home'])
+def get_movie(id:int):                   # para imprimir el valor id debemos indicarlo también dentro de la f(añadirlo como parámetro)
+    for movie in movies:
+        if movie['id'] == id:
+            return movie
+    return[]
+
+
+
+@app.get('/movies/', tags=['Home'])       #ya no filtro por id (/movies/ esta va tener parámetros query)
+def get_movie_by_category(category:str, year:int):      # defino param por query, es añadiendo dentro de la f()
+             
+    for movie in movies:
+        if movie['category'] == category:
+            return movie
+    return[]
